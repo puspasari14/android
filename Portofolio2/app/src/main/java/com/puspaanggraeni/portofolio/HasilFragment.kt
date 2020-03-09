c6023361be2175dac50370e02f94534297184a2f
puspasari14 <puspasaridiananggraeni@gmail.com>
2020-03-09 16:54:04 +0700

    portofolio

---
+++ Portofolio2/app/src/main/java/com/puspaanggraeni/portofolio/HasilFragment.kt
@@ -0,0 +1,24 @@
+package com.puspaanggraeni.portofolio
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HasilFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hasil, container, false)
+    }
+
+
+}
---
+++ Portofolio2/app/src/main/java/com/puspaanggraeni/portofolio/InputFragment.kt
@@ -0,0 +1,36 @@
+package com.puspaanggraeni.portofolio
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_input.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class InputFragment : Fragment() {
+    lateinit var nav: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_input, container, false)
+    }
+    overide fun onViewCreated(view: View, savedInstanceState: Bundle?){
+
+    super.onViewCreated(view, savedInstanceState)
+        nav = Navigation.findNavController(view)
+        buttonsubmit.setOnClickListener{it:View!
+        if(editTextinput1.text.toString().isNotEmpty()){
+            val input : string
+        }}
+    }
+
+}
